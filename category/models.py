1ae40a75aec2bd65e8f592e43f45fba7eb9f1238
HitzCommitted <hitzcommitted@gmail.com>
2023-10-13 10:12:08 +0300

    working category

--- category/models.py
+++ category/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     category_name = models.CharField(max_length=200, unique=True)
-    slug = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField(max_length=200, blank=True)
     category_image = models.ImageField(upload_to="photos/categories", blank=True)
 
